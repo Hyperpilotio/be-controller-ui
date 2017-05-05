f58bb3c446f8dc3fbf5d3226a1121809d807cfa8
adrianliaw <adrianliaw2000@gmail.com>
2017-05-06 00:52:24 +0800

    Allow multi-columns settings view

--- components/Settings.js
+++ components/Settings.js
@@ -6,28 +6,38 @@ import {
   TableRow,
   TableRowColumn
 } from "material-ui/Table"
+import { Row, Col } from "react-grid-system"
 
 import _ from "lodash"
 
 
-export default ({settings, omit}) => (
-  <Table>
-    <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
-      <TableRow>
-        <TableHeaderColumn>Option</TableHeaderColumn>
-        <TableHeaderColumn>Value</TableHeaderColumn>
-      </TableRow>
-    </TableHeader>
-    <TableBody displayRowCheckbox={false}>
-      {_.map(
-        _.omit(settings, omit),
-        (val, key) => (
-          <TableRow key={key}>
-            <TableRowColumn>{key}</TableRowColumn>
-            <TableRowColumn>{_.toString(val)}</TableRowColumn>
-          </TableRow>
-        )
-      )}
-    </TableBody>
-  </Table>
-)
+export default ({settings, omit = [], cols = 1}) => {
+  settings = _.omit(settings, omit)
+  return <Row>
+    {_.range(cols).map(i =>
+      <Col md={12 / cols}>
+        <Table>
+          <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
+            <TableRow>
+              <TableHeaderColumn>OPTION</TableHeaderColumn>
+              <TableHeaderColumn>VALUE</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody displayRowCheckbox={false}>
+            {
+              _.filter(
+                _.entries(settings),
+                (vals, j) => _.includes(_.range(i, _.size(settings), cols), j)
+              ).map(([key, val]) =>
+                <TableRow key={key}>
+                  <TableRowColumn>{key}</TableRowColumn>
+                  <TableRowColumn>{_.toString(val)}</TableRowColumn>
+                </TableRow>
+              )
+            }
+          </TableBody>
+        </Table>
+      </Col>
+    )}
+  </Row>
+}
